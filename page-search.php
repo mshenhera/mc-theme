f4f83ecd26dfde5cf6106914236f0ddfe0531bf9
mykola shenhera <mshenhera@mykolas-MacBook-Pro.local>
2015-08-23 10:54:32 +0300

    added main prof search page

--- page-search.php
+++ page-search.php
@@ -16,15 +16,9 @@ get_header('directory-search'); ?>
 Select Country:   <select id="country2" name ="country2"></select>
 <script language="javascript">
 populateCountries("country2");
--->
 
-Select Country (with states):   <select id="country" name ="country"></select>
-Select State: <select name ="state" id ="state"></select>
- <script language="javascript">
-populateCountries("country", "state");
- </script>
 
- </script>
+ </script>-->
 	<div id="primary" class="content-area nosidebar">
 		<div id="content" class="site-content" role="main">
 
@@ -45,6 +39,8 @@ populateCountries("country", "state");
 						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'i-max' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
 					</div><!-- .entry-content -->
 
+<?php get_template_part('masters_city/main_search', 'page'); ?>
+
 					<footer class="entry-meta">
 						<?php edit_post_link( __( 'Edit', 'i-max' ), '<span class="edit-link">', '</span>' ); ?>
 					</footer><!-- .entry-meta -->
@@ -55,6 +51,9 @@ populateCountries("country", "state");
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
-
-
+<!--
+ <script language="javascript">
+populateCountries("uultra-search-input-Region", "uultra-search-input-City");
+ </script>
+-->
 <?php get_footer(); ?>
