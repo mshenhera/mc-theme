7a20d4a869f4ea4b26ffdef542150f8985195d56
mykola shenhera <mshenhera@mykolas-MacBook-Pro.local>
2015-08-23 22:00:35 +0300

    Removed Test mark from header.php

--- header.php
+++ header.php
@@ -19,7 +19,7 @@ $top_phone = esc_attr(get_theme_mod('top_phone', '1-000-123-4567'));
 $top_email = esc_attr(get_theme_mod('top_email', 'email@i-create.com'));
 $imax_logo = get_theme_mod( 'logo', get_template_directory_uri().'/images/logo.png' );
 
-global $post; 
+global $post;
 
 ?>
 
@@ -28,7 +28,7 @@ global $post;
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
-	<?php    
+	<?php
     if ( ! function_exists( '_wp_render_title_tag' ) ) :
         function imax_render_title() {
     ?>
@@ -36,15 +36,14 @@ global $post;
     <?php
         }
         add_action( 'wp_head', 'imax_render_title' );
-    endif;    
-    ?> 
+    endif;
+    ?>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> style=" <?php echo $bg_style; ?> ">
 	<div id="page" class="hfeed site">
-TEST_T    	
         <?php if ( $top_email || $top_phone || imax_social_icons() ) : ?>
     	<div id="utilitybar" class="utilitybar">
         	<div class="ubarinnerwrap">
@@ -57,17 +56,17 @@ TEST_T
                     <?php _e('Call us : ','i-max'); ?> <?php echo esc_attr($top_phone); ?>
                 </div>
                 <?php endif; ?>
-                
+
                 <?php if ( !empty($top_email) ) : ?>
                 <div class="topphone">
                     <i class="topbarico genericon genericon-mail"></i>
                     <?php _e('Mail us : ','i-max'); ?> <?php echo sanitize_email($top_email); ?>
                 </div>
-                <?php endif; ?>                
-            </div> 
+                <?php endif; ?>
+            </div>
         </div>
         <?php endif; ?>
-        
+
         <div class="headerwrap">
             <header id="masthead" class="site-header" role="banner">
          		<div class="headerinnerwrap">
@@ -79,61 +78,61 @@ TEST_T
                         <span id="site-titlendesc">
                             <a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
                                 <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-                                <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>   
+                                <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
                             </a>
                         </span>
-                    <?php endif; ?>	
-        
+                    <?php endif; ?>
+
                     <div id="navbar" class="navbar">
                         <nav id="site-navigation" class="navigation main-navigation" role="navigation">
                             <h3 class="menu-toggle"><?php _e( 'Menu', 'i-max' ); ?></h3>
                             <a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'i-max' ); ?>"><?php _e( 'Skip to content', 'i-max' ); ?></a>
-                            <?php 
+                            <?php
 								if ( has_nav_menu(  'primary' ) ) {
 										wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'container_class' => 'nav-container', 'container' => 'div' ) );
 									}
 									else
 									{
-										wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-container' ) ); 
+										wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-container' ) );
 									}
 								?>
-							
+
                         </nav><!-- #site-navigation -->
-                        <div class="topsearch">
-                            <?php get_search_form(); ?>
-                        </div>
+       <!--                 <div class="topsearch">
+                            <?php // get_search_form(); ?>
+                        </div> -->
                     </div><!-- #navbar -->
                     <div class="clear"></div>
                 </div>
             </header><!-- #masthead -->
         </div>
-        
+
         <!-- #Banner -->
         <?php
-		
+
 		$hide_title = rwmb_meta('imax_hidetitle');
 		$show_slider = rwmb_meta('imax_show_slider');
 		$other_slider = rwmb_meta('imax_other_slider');
 		$custom_title = rwmb_meta('imax_customtitle');
-		
+
 		$hide_front_slider = get_theme_mod('slider_stat', 1);
 		$other_front_slider = get_theme_mod('blogslide_scode', '');
 		$itrans_slogan = esc_attr(get_theme_mod('banner_text', ''));
-		
-		
+
+
 		$other_slider = esc_html($other_slider);
-		$other_front_slider = esc_html($other_front_slider);			
-		
+		$other_front_slider = esc_html($other_front_slider);
+
 		if($other_slider) :
 		?>
-		
+
         <div class="other-slider" style="">
 	       	<?php echo do_shortcode( htmlspecialchars_decode($other_slider) ) ?>
         </div>
         <?php //elseif ( is_front_page() )  : ?>
-		
-		<?php	
-		elseif ( is_home() && !is_paged() || $show_slider ) : 
+
+		<?php
+		elseif ( is_home() && !is_paged() || $show_slider ) :
 		?>
             <?php //imax_ibanner_slider(); ?>
             <?php if (!empty($other_front_slider)) : ?>
@@ -148,19 +147,19 @@ TEST_T
                             if ($itrans_slogan) {
                                 echo esc_attr($itrans_slogan);
                             }
-                        ?>	                 
+                        ?>
                     </h1>
                 </div>
-            </div>                                    
-        	<?php endif; ?>            
-            
-        <?php 
-		elseif(!$hide_title) : 
+            </div>
+        	<?php endif; ?>
+
+        <?php
+		elseif(!$hide_title) :
 		?>
-        
+
         <div class="iheader" style="">
         	<div class="titlebar">
-            	
+
                 <?php
 					if( is_archive() )
 					{
@@ -171,16 +170,16 @@ TEST_T
 								printf( __( 'Monthly Archives: %s', 'i-max' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'i-max' ) ) );
 							elseif ( is_year() ) :
 								printf( __( 'Yearly Archives: %s', 'i-max' ), get_the_date( _x( 'Y', 'yearly archives date format', 'i-max' ) ) );
-							elseif ( is_category() ) :	
-								printf( __( 'Category Archives: %s', 'i-max' ), single_cat_title( '', false ) );		
+							elseif ( is_category() ) :
+								printf( __( 'Category Archives: %s', 'i-max' ), single_cat_title( '', false ) );
 							else :
 								_e( 'Archives', 'i-max' );
-							endif;                						
+							endif;
 						echo '</h1>';
 					} elseif ( is_search() )
 					{
 						echo '<h1 class="entry-title">';
-							printf( __( 'Search Results for: %s', 'i-max' ), get_search_query() );					
+							printf( __( 'Search Results for: %s', 'i-max' ), get_search_query() );
 						echo '</h1>';
 					} else
 					{
@@ -193,17 +192,17 @@ TEST_T
 							echo '<h1 class="entry-title">';
 							the_title();
 							echo '</h1>';
-						}						
+						}
 					}
-					
+
 					//echo rwmb_meta('imax_customtitle');
 
 					/**/
             	?>
-				<?php 
-				
+				<?php
+
 					$hide_breadcrumb = rwmb_meta('imax_hide_breadcrumb');
-					
+
                     if(function_exists('bcn_display') && !$hide_breadcrumb )
                     {
 				?>
@@ -212,13 +211,13 @@ TEST_T
                         bcn_display();
 				?>
                 	</div>
-                <?php		
-                    } 
-                ?>               
-            	
+                <?php
+                    }
+                ?>
+
             </div>
         </div>
-        
+
 		<?php endif; ?>
 		<div id="main" class="site-main">
 
